cf3ba97dbd624b130df8e4be5512076afbf4100b
Donovan C. Young <dyoung522@gmail.com>
2015-09-22 21:05:34 -0400

    add rspec support

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,10 @@
-class ApplicationController < ActionController::Base
+
+class ApplicationController < ActionController::API
+  # include ActionController::Serialization
+
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  # protect_from_forgery with: :exception
 
   before_action :set_locale
 
