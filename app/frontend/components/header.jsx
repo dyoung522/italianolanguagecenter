70e39b256d189bb05e151b0f20d51c556a56c8c7
Donovan C. Young <dyoung522@gmail.com>
2015-10-15 08:30:37 -0400

    Add buttons to switch locale

--- app/frontend/components/header.jsx
+++ app/frontend/components/header.jsx
@@ -2,6 +2,10 @@ import React    from 'react';
 import ReactDOM from 'react-dom';
 
 var Header = React.createClass({
+  changeLocale: function(locale) {
+    return function() { I18n.locale = locale; };
+  },
+
   _onPageScrollClick: function(event) {
     var anchor = ReactDOM.findDOMNode(event.target);
 
@@ -13,6 +17,7 @@ var Header = React.createClass({
   },
 
   // TODO: Refactor this using React (esp. for sections)
+  // TODO: Add language selection links
   render: function() {
     return (
       <nav className="navbar navbar-default navbar-fixed-top" role="navigation">
@@ -34,14 +39,22 @@ var Header = React.createClass({
           <div className="collapse navbar-collapse navbar-ex1-collapse">
             <ul className="nav navbar-nav">
               <li className="hidden">
-                <a className="page-scroll" href="#page-top"></a>
+                <a className="page-scroll" href="#page-top" />
               </li>
               <li><a onClick={this._onPageScrollClick} href="#welcome">{I18n.t('menu.intro')}</a></li>
               <li><a onClick={this._onPageScrollClick} href="#classes">{I18n.t('menu.classes')}</a></li>
             </ul>
             <ul className="nav navbar-nav navbar-right">
+              <li><a className="login" href="#login">{I18n.t('menu.login')}</a></li>
               <li>
-                <a className="login" href="#login">{I18n.t('menu.login')}</a>
+                <a type="button" className="language-flag" href="?locale=en" onClick={this.changeLocale('en')}>
+                  <img src="/assets/english-flag.png" alt="english"/>
+                </a>
+              </li>
+              <li>
+                <a type="button" className="language-flag" href="?locale=it" onClick={this.changeLocale('it')}>
+                  <img src="/assets/italian-flag.png" alt="italiano"/>
+                </a>
               </li>
             </ul>
           </div>
